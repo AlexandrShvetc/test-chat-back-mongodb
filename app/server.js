a47e3dc23feac0940131429a89c835f780d92804
Alex Shvets <91091399+bx2924bk@users.noreply.github.com>
2022-04-01 10:34:12 +0300

    add mongo db

--- app/server.js
+++ app/server.js
@@ -127,6 +127,7 @@ app.post("/pusher/auth/signing", (req, res) => {
     const checkUser = collection.findOne({email: req.body.email}, function (err, email) {
         if (err) return console.log(err);
         // res.send(email);
+        return email;
     })
     // const error = {
     //     err: checkUser
