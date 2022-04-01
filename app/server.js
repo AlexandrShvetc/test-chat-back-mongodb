f3464953b180b18054ecb16a3f1cf1505872a8e2
Alex Shvets <91091399+bx2924bk@users.noreply.github.com>
2022-04-01 10:58:25 +0300

    add mongo db

--- app/server.js
+++ app/server.js
@@ -41,7 +41,7 @@ const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: 
 
 // Allow CORS
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
